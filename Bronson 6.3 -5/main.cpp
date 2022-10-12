d6dcdfe6482362bf11dcf51d569252a2bcc77ad8
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-12 21:35:22 +0000

    Thats why it felt easy, it wasnt done lol

--- Bronson 6.3 -5/main.cpp
+++ Bronson 6.3 -5/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int time(int seconds){
   int hours, mins, secs;
   secs = seconds;
-  mins = (seconds / 60);
+  cout << "The time in seconds is: " << secs << endl;
+  mins = (seconds / 60);  
+  cout << "The time in minutes is: " << mins << endl;
   hours = (seconds / 3600);
+  cout << "The time in hours is: " << hours << endl;
   return;
+}
+
+int main (){
+  int seconds;
+  cout << "Input a time in seconds: ";
+  cin >> seconds; 
+
+  time(seconds);
 }
