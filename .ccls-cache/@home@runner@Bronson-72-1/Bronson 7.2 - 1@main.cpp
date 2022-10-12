6a9db21f5d5a89beaf8c8a58b353715e6072059b
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-12 22:16:39 +0000

    didnt really need it for the last question, so now its for number 4

---
+++ .ccls-cache/@home@runner@Bronson-72-1/Bronson 7.2 - 1@main.cpp
@@ -0,0 +1,15 @@
+//Samuel Peel
+//CSCI 130
+//10-12-22
+#include <iostream>
+using namespace std;
+
+int main() {
+  double list[100] = {6.29, 6.95, 7.25, 7.35, 7.40, 7.42};
+  int index;
+  
+  for (index = 0; index < 100; index++)
+    cout << list[index] << " ";
+
+  cout << endl;
+}
