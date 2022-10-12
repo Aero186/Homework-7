896b083cded62c165292e1485385e59c40beb58f
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-12 23:05:24 +0000

    spruced it up a bit

--- Bronson 7.2 - 4/main.cpp
+++ Bronson 7.2 - 4/main.cpp
@@ -10,24 +10,25 @@ int main() {
   double current[5];
   double power[5];
 
-  cout << "Input 5 values (seperated by a space) for current: ";
+  cout << "--Input 5 values (seperated by a space) for current: ";
     int index;
     for(index = 0; index < 5; index++){
     cin >> current[index];}
 
-  cout << "Resistance values are: " << endl;
+  cout << "--Resistance values are: " << endl;
     for (index = 0; index < 5; index++){
     cout << resistance[index] << " ";
     cout << endl; }
   
-  cout << "Current values are: " << endl;
+  cout << "--Current values are: " << endl;
    for (index = 0; index < 5; index++){
     cout << current[index] << " ";
     cout << endl; }
 
-  cout << "Power values are: " << endl;
+  cout << "--Power values are: " << endl;
     for(index = 0; index < 5; index++){
       power[index] = ( resistance[index] * pow( current[index], 2 ) );
     cout << power[index] << " ";
     cout << endl; }
+return 0;
 }
