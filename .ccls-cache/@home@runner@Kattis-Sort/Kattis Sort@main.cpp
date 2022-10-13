b1af354544ecbe48b4510e60ee25312937114809
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-13 01:59:16 +0000

    the sort kinda works, its just a little out of order for Kattis when there is a repeat input, but its really a comparable outcome...

--- .ccls-cache/@home@runner@Kattis-Sort/Kattis Sort@main.cpp
+++ .ccls-cache/@home@runner@Kattis-Sort/Kattis Sort@main.cpp
@@ -4,40 +4,60 @@
 #include <iostream>
 using namespace std;
 
+// C++ program for insertion sort
+
+#include <bits/stdc++.h>
+
+// Function to sort an array using
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+		// Move elements of arr[0..i-1],
+		// that are greater than key, to one
+		// position ahead of their
+		// current position
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+// A utility function to print an array
+// of size n
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// This is code is contributed by rathbhupendra
+
+
 int main() {
   int N, C;
     cin >> N;
     cin >> C;
   int index;
-  //cin N >> C >> endl;
+
   int list[N];
 
   cout << "Enter " << N << " integers that are less than " << C << ": "; // <-- comment out for use in kattis
   
-  for(index = 0; index < N; index++)
+  for(index = 0; index < N; index++){
     cin >> list[index];
-
-
-int BiggestIndex, loc, temp;
-  
-//increment throught the main array
-  for (index = 0; index < N; index++){
-    BiggestIndex = index;
-    
-//incrememnt through each comparison
-    for ( loc = index + 1; loc < N; loc++)
-      if ( list[loc] > list[BiggestIndex] ){
-        BiggestIndex = loc; // location of new smallest index
     }
-    
-    // make the swap
-    temp = list[BiggestIndex];
-    list[BiggestIndex] = list[index];
-    list[index] = temp;
-  }
 
-  for (index = 0; index < N; index++)
-    cout << list[index] << " ";
-
-  cout << endl;
+  insertionSort(list, N);
+  printArray(list, N);
 }
